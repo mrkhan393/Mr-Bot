5ec190e8fbe55db921418702b0371ce4af8a3645
M R Khan Dipu <175508274+mrkhan393@users.noreply.github.com>
2025-04-26 01:36:27 +0600

    Update session_manager.py

--- session_manager.py
+++ session_manager.py
@@ -71,17 +71,17 @@ class ChatBotManager:
         except Exception as e:
             return f"Error generating response: {str(e)}"
         
-    def end_session(self, session_id):
+     def end_session(self, session_id):
         if session_id in self.sessions:
             summary = self.sessions[session_id].get_summary()
             print(f"\n Session Ended for {summary['user']}")
             print(f"Duration: {summary['duration']}")
             print("Chat Summary:")
-            for msg in summary["messages"]:  # ✅ FIXED
+            for msg in summary["message"]:
                 print(f"{msg['time']} - {msg['sender']}: {msg['text']}")
-            del self.sessions[session_id]
+                del self.sessions[session_id]
         else:
-            print("Invalid session ID.")
+            print("Invalid Session ID.")
 
 
 
